8fd30834a86f6f90e565469c701fa599c8c76776
Thomas Breland <thomas@breland.tech>
2025-03-10 09:54:25 -0400

    add base path

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "./",
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
