ba52e8f0a529bb220ecf53649306bb9d28c1cb5b
Thomas Breland <thomas@breland.tech>
2025-03-11 13:56:00 -0400

    remove vite-plugin-html again

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -2,7 +2,6 @@ import path from "path";
 
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 // allow base URL to be set by environment, e.g., during pipeline, for deploying to GitHub Pages subdirectory
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.GH_PAGES_BASE_URL : '/';
