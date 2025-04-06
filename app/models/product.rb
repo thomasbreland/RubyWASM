1ee431db2eaf53fc63037484b74b957e230547a9
Thomas Breland <thomas@breland.tech>
2025-04-06 14:51:21 -0400

    remove rich text and update README

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 
   has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
-  has_rich_text :description
+  attribute :description, :text
 
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
