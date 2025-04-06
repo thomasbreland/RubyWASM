f232cbd5bdf5eb01c1f94fe0939a1ba615020042
Thomas Breland <thomas@breland.tech>
2025-04-06 14:51:03 -0400

    seed demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  email_address: "you@example.org",
+  password: "s3cr3t",
+  password_confirmation: "s3cr3t"
+)
